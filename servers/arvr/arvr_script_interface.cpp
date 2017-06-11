b33117fa46c74be9a981336aa64843563e0ddac7
Andreas Haas <liu.gam3@gmail.com>
2017-06-11 10:54:29 +0200

    ArVrScriptInterface: Add virtual method binds.

--- servers/arvr/arvr_script_interface.cpp
+++ servers/arvr/arvr_script_interface.cpp
@@ -33,14 +33,18 @@ bool ArVrScriptInterface::is_stereo() {
 }
 
 bool ArVrScriptInterface::is_initialized() {
-	return false;
+	ERR_FAIL_COND_V(!(get_script_instance() && get_script_instance()->has_method("is_initialized")), false);
+	return get_script_instance()->call("is_initialized");
 }
 
 bool ArVrScriptInterface::initialize() {
-	return false;
+	ERR_FAIL_COND_V(!(get_script_instance() && get_script_instance()->has_method("initialize")), false);
+	return get_script_instance()->call("initialize");
 }
 
 void ArVrScriptInterface::uninitialize() {
+	ERR_FAIL_COND(!(get_script_instance() && get_script_instance()->has_method("uninitialize")));
+	get_script_instance()->call("uninitialize");
 }
 
 Size2 ArVrScriptInterface::get_recommended_render_targetsize() {
@@ -71,5 +75,18 @@ void ArVrScriptInterface::process(float p_delta_time) {
 }
 
 void ArVrScriptInterface::_bind_methods() {
-	//ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::BOOL, "is_installed"));
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::BOOL, "is_installed"));
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::BOOL, "hmd_is_present"));
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::BOOL, "supports_hmd"));
+
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::BOOL, "is_initialized"));
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::BOOL, "initialize"));
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo("uninitialize"));
+
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::BOOL, "is_stereo"));
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::VECTOR2, "get_recommended_render_targetsize"));
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo(Variant::TRANSFORM, "get_transform_for_eye", PropertyInfo(Variant::INT, "eye"), PropertyInfo(Variant::TRANSFORM, "cam_transform")));
+	//ClassDB::add_virtual_method(get_class_static(), MethodInfo("get_projection_for_eye"));
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo("commit_for_eye", PropertyInfo(Variant::INT, "eye"), PropertyInfo(Variant::_RID, "render_target")));
+	ClassDB::add_virtual_method(get_class_static(), MethodInfo("process", PropertyInfo(Variant::REAL, "delta")));
 }
